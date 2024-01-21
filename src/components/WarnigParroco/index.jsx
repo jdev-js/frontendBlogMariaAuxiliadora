a471d214463486564fc7173a90a4b589c3aae171
jdev <jesusalcalarojas@gmail.com>
2024-01-21 15:08:34 -0400

    modificate color and text

--- src/components/WarnigParroco/index.jsx
+++ src/components/WarnigParroco/index.jsx
@@ -1,22 +1,24 @@
-import styles from './style.module.css'
-import UserNurse from '../Icons/UserNurse'
-import imgURL from './../../asset/image2.png'
+import styles from "./style.module.css"
+import UserNurse from "../Icons/UserNurse"
+import imgURL from "./../../asset/image2.png"
 
 export default function WarnigParroco() {
   return (
     <div className={styles.containerWarnig}>
-      <UserNurse width={80} height={80} fill={'#88d4f2'}/>
+      <UserNurse width={80} height={80} fill={"#88d4f2"} />
       <h4 className={styles.titleText}>Crea una reseña del sacerdote actual</h4>
-      <span className={styles.descritionText}>Tambien puedes publicar una reseña del parroco anterior (opcional)</span>
+      <span className={styles.descritionText}>
+        Tambien puedes publicar una reseña del parroco anterior (opcional)
+      </span>
     </div>
   )
 }
 
-export function NotParroco(){
-  return(
+export function NotParroco() {
+  return (
     <div className={styles.containerNotArticulo}>
-    <img src={imgURL}/>
-    <h4>Sin hay parrocos disponibles</h4>
-  </div>
+      <img src={imgURL} />
+      <h4>Sin parrocos disponibles</h4>
+    </div>
   )
 }
